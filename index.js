fd7f52fa6b96cd38432ce85cc0c2a3bbfb1c6748
Rene Nvt <brnvtrene@gmail.com>
2017-10-19 16:49:07 -0500

    Este es mi primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//console.log('hola node ');
+
+const lodash = require('lodash');
+const obj={
+  nombre: 'Rene Nvt',
+  edad: 19,
+  email: 'brnvtrene@gmail.com',
+  tw: '@itsrenenvt'
+}
+
+console.log(obj.email);
+
+/*
+
+function hola(){
+  return 'hola mundo x2'
+}
+console.log(hola());
+console.log(2+2);
+
+var lista =[1,2,3,4,5]
+console.log(lista);*/
